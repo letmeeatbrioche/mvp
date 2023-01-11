f413d6b708dbf3c3234872b9b4c2dd6625e4ad13
Haillie W <hailliewilloughby@gmail.com>
2023-01-11 00:02:54 -0500

    Add more cat sound effects and click functions

--- my-app/src/components/Frame.js
+++ my-app/src/components/Frame.js
@@ -4,15 +4,22 @@ import Cat2 from './Cat2';
 import Cat3 from './Cat3';
 import Cat4 from './Cat4';
 import Cat5 from './Cat5';
-// import trill from '../cat-purr-twit4.wav';
-import meow from '../fiji-meow-02.wav';
+import trill from '../cat-purr-twit4.wav';
+import meow1 from '../fiji-meow-02.wav';
+import meow2 from '../cat-1-meow.mp3'
 
 const Frame = () => {
-  // const catTrill = new Audio(trill);
-  const catMeow = new Audio(meow);
-  const makeSound = (event) => {
-    // catTrill.play();
-    catMeow.play();
+  const makeMeow1 = (event) => {
+    const catMeow1 = new Audio(meow1);
+    catMeow1.play();
+  }
+  const makeMeow2 = (event) => {
+    const catMeow2 = new Audio(meow2);
+    catMeow2.play();
+  }
+  const makeTrill = (event) => {
+    const catTrill = new Audio(trill);
+    catTrill.play();
   }
   const catSprite1 = useRef(null);
   const catSprite2 = useRef(null);
@@ -25,33 +32,33 @@ const Frame = () => {
     cat.current.style.top = Math.floor(Math.random()*(80 - 40) + 40)+'%';
     cat.current.style.left = Math.floor(Math.random()*55+10)+'%';
   }
-  useEffect(() => {
-    const interval = setInterval(() => {
-      moveCat(catSprite1);
-      moveCat(catSprite2);
-      moveCat(catSprite3);
-      moveCat(catSprite4);
-      moveCat(catSprite5);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     moveCat(catSprite1);
+  //     moveCat(catSprite2);
+  //     moveCat(catSprite3);
+  //     moveCat(catSprite4);
+  //     moveCat(catSprite5);
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div className="App">
       <div className="frame">
-        <div className="rrs-container" ref={catSprite1} onClick={makeSound}>
+        <div className="rrs-container" ref={catSprite1} onClick={makeMeow1}>
           <Cat1 />
         </div>
-        <div className="rrs-container" ref={catSprite2} onClick={makeSound}>
+        <div className="rrs-container" ref={catSprite2} onClick={makeMeow2}>
           <Cat2 />
         </div>
-        <div className="rrs-container" ref={catSprite3} onClick={makeSound}>
+        <div className="rrs-container" ref={catSprite3} onClick={makeTrill}>
           <Cat3 />
         </div>
-        <div className="rrs-container" ref={catSprite4} onClick={makeSound}>
+        <div className="rrs-container" ref={catSprite4} onClick={makeTrill}>
           <Cat4 />
         </div>
-        <div className="rrs-container" ref={catSprite5} onClick={makeSound}>
+        <div className="rrs-container" ref={catSprite5} onClick={makeMeow1}>
           <Cat5 />
         </div>
       </div>
