a078384b272d05dcef6196a81f2126eab01565d7
Haillie W <hailliewilloughby@gmail.com>
2023-01-10 20:30:19 -0500

    Add cat sound to cat onClick function

--- my-app/src/components/Frame.js
+++ my-app/src/components/Frame.js
@@ -4,11 +4,16 @@ import Cat2 from './Cat2';
 import Cat3 from './Cat3';
 import Cat4 from './Cat4';
 import Cat5 from './Cat5';
+import trill from '../262309__steffcaffrey__cat-purr-twit4.wav';
 
 const Frame = () => {
+  const audio = new Audio(trill);
+  const makeSound = (event) => {
+    audio.play();
+  }
   return (
     <div className="App">
-      <div className="frame">
+      <div className="frame" onClick={makeSound}>
         <div className="rrs-container">
           <Cat1 />
         </div>
