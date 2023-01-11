b09921f1af03a7eafd2fcffd25c9f10bfa37331c
Haillie W <hailliewilloughby@gmail.com>
2023-01-11 15:36:38 -0500

    Remove background music code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import Frame from './components/Frame';
-import bgMusic from './Frederic_Lardon_feat_Laura_Palme_-_01_-_Dans_la_dtente_au_magasin_de_sport.mp3';
 
 function App() {
-  const playMusic = () => {
-    const music = new Audio(bgMusic);
-    music.loop = true;
-    music.play();
-  }
   return (
     <div>
       <Frame />
-      <button className="button-45" onClick={playMusic}>Play Music</button>
     </div>
   );
 }
