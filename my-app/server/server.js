833b3219bd0ca9cadcc6a062bcb3ce3a0b1e0cf3
Haillie W <hailliewilloughby@gmail.com>
2023-01-11 19:33:04 -0500

    Remove comments

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -9,11 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// TO-DO
-/**
- * Create a caching system:
- * Store keys as numbers of 5 digits with leading zeros(?)
- * Store key values as hashes with a hashing function
- * Store key-value cache pairs in a database (mongo?)
- */
