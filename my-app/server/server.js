c65df9fc14c11ce6962809b808281b9c7a1d70ed
Haillie W <hailliewilloughby@gmail.com>
2023-01-11 19:40:34 -0500

    Change port reference variable, no longer exists

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.listen(port, () => {
+app.listen(process.env.SERVERPORT, () => {
   console.log(`Example app listening on port ${process.env.SERVERPORT}`);
 });
